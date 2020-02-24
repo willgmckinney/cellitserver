e15d6f8ba0f5d7f3ee2a4c7876ab7e385dc3d049
Douglas Brown <douglasbrown@Douglass-MacBook-Air.local>
2020-02-24 16:44:12 -0500

    updated server

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -5,67 +5,67 @@ let User = sequelize.import('../models/user');
 let bcrypt = require('bcryptjs');
 let jwt = require('jsonwebtoken');
 
-router.post('/signup', function (req, res) {
-    User.create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 13),
-        street: req.body.street,
-        city: req.body.city,
-        zip: req.body.zip,
-        phone: req.body.phone,
-        admin: req.body.admin
-    }).then(
-        function createSuccess(user){
+router.post('/signup', function(req, res) {
+  User.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 13),
+    street: req.body.street,
+    city: req.body.city,
+    zip: req.body.zip,
+    phone: req.body.phone,
+    admin: req.body.admin
+  }).then(
+    function createSuccess(user) {
+      let token = jwt.sign({ id: user.id }, process.env.JWT, {
+        expiresIn: 60 * 60 * 24
+      });
 
-            let token = jwt.sign({id: user.id}, process.env.JWT, {expiresIn: 60*60*24});
-
-            res.json({
-                user: user,
-                message: 'created',
-                sessionToken: token
-            });
-        },
-        function createError(err) {
-            res.send(500, err.message);
-        }
-    );
+      res.json({
+        user: user,
+        message: 'created',
+        sessionToken: token
+      });
+    },
+    function createError(err) {
+      res.send(500, err.message);
+    }
+  );
 });
 
 router.post('/signin', function(req, res) {
-
-    User.findOne( { where: {email: req.body.email}}).then (
-        function(user) {
-            if (user) {
-                bcrypt.compare(req.body.password, user.password, function(err, matches) {
-                    if(matches){
-                        let token = jwt.sign({id: user.id}, process.env.JWT, {expiresIn: 60*60*24});
-                        res.json({
-                            user: user,
-                            message: "succesfully authenticated",
-                            sessionToken: token
-                        });
-                    } else {
-                        res.status(502).send({error: "Password"})
-                    }
-                });
-            } else {
-                res.status(502).send({error: "Username"})
-            }
-        },
-    );
+  User.findOne({ where: { email: req.body.email } }).then(function(user) {
+    if (user) {
+      bcrypt.compare(req.body.password, user.password, function(err, matches) {
+        if (matches) {
+          let token = jwt.sign({ id: user.id }, process.env.JWT, {
+            expiresIn: 60 * 60 * 24
+          });
+          res.json({
+            user: user,
+            message: 'succesfully authenticated',
+            sessionToken: token
+          });
+        } else {
+          res.status(502).send({ error: 'Password' });
+        }
+      });
+    } else {
+      res.status(502).send({ error: 'Username' });
+    }
+  });
 });
 
 router.get('/', function(req, res) {
-    User.findOne({where: {id : req.user.id}, include: ['cart', 'order']}).then(
-        function findAllSuccess(data) {
-            res.json(data);
-        },
-        function findAllError(err) {
-            res.send(500, err.message);
-        }
-    );
-})
+  User.findOne({ where: { id: req.user.id }, include: ['cart', 'order'] }).then(
+    function findAllSuccess(data) {
+      res.json(data);
+    },
+    function findAllError(err) {
+      res.send(500, err.message);
+    }
+  );
+});
 
 module.exports = router;
