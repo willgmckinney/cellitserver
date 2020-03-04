8af656dd1e86f3a44ff0333e49cbb7d2035680a7
William Mckinney <williammckinney@Williams-MacBook-Pro.local>
2020-03-04 13:33:06 -0500

    changed server

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('cellit', 'postgres', process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
